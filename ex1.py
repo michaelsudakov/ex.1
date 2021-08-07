5586146897904b33a87967f935e8bab68b44895d
michael sudakov <michaelsudakov@gmail.com>
2021-08-07 16:17:19 +0300

    solved the assiment

--- ex1.py
+++ ex1.py
@@ -1,3 +1,13 @@
-if __name__ == '__main__':
-    print("5")
-    print("5")
+
+
+def avg():
+    counter = 0
+    sum = 0
+    num = input("enter num if you want to stop enter q ")
+    while num != "q" :
+        sum += float(num)
+        counter += 1
+        num = input("enter num ")
+    return sum/counter
+
+print(avg())
