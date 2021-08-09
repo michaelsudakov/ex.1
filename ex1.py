d1939efb5ff7a5a22f0d0255f760d3c8af6a7050
michael sudakov <michaelsudakov@gmail.com>
2021-08-09 17:22:20 +0300

    FIxed A

--- ex1.py
+++ ex1.py
@@ -1,13 +1,18 @@
 
+try:
+    def calculate_average ():
+        counter = 0
+        sum = 0
+        function_stop = "q"
+        num = input("enter num if you want to stop enter " + function_stop + " ")
+        while num != function_stop :
+            sum += float(num)
+            counter += 1
+            num = input("enter num ")
+        return sum/counter
+    print(calculate_average())
+except:
+    print("bad input")
 
-def avg():
-    counter = 0
-    sum = 0
-    num = input("enter num if you want to stop enter q ")
-    while num != "q" :
-        sum += float(num)
-        counter += 1
-        num = input("enter num ")
-    return sum/counter
 
-print(avg())
+
