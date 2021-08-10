a16c6bffbeedd3b5669a90a3078085e7f0fb942b
michael sudakov <michaelsudakov@gmail.com>
2021-08-10 16:35:40 +0300

    Fixed D

--- ex1D.py
+++ ex1D.py
@@ -1,4 +1,10 @@
-Num_List = [2, 12, 100, 6, 8, 3]
-divisor = 10
+from typing import List
 
-print(list(filter(lambda numbers: numbers % divisor != 0, Num_List)))
+
+def remove_dividend_numbers(num_list: List[int], divisor: int) -> List[int]:
+    num_list = (list(filter(lambda numbers: numbers % divisor != 0, num_list)))
+    return num_list
+
+
+list1 = remove_dividend_numbers([2, 9, 100, 6, 8, 3], 2)
+print(list1)
