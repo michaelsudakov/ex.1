bc2c1861fe6e6b949899cd1373880d12f71ad048
michael sudakov <michaelsudakov@gmail.com>
2021-08-10 13:15:02 +0300

    fixed C

--- ex1C.py
+++ ex1C.py
@@ -1,8 +1,7 @@
-def remove_dividend_numbers(num_list: list[int], divisor: int):
+from typing import List
+def remove_dividend_numbers(num_list: List[int], divisor: int) -> List[int]:
     return [number for number in num_list if number % divisor != 0]
 
-
-
-list1 = remove_dividend_numbers([2, 12, 100, 6, 8, 3], 2)
+list1 = remove_dividend_numbers([2, 12, 100, 6, 8, 3], 4)
 print(list1)
 
