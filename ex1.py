a498bcd1a49d17c0fd235d94e1c96e331c9fcf25
michael sudakov <michaelsudakov@gmail.com>
2021-08-10 12:51:13 +0300

    fixed A

--- ex1.py
+++ ex1.py
@@ -1,18 +1,22 @@
+FUNCTION_STOP_CHAR = "q"
 
-try:
-    def calculate_average ():
-        counter = 0
-        sum = 0
-        function_stop = "q"
-        num = input("enter num if you want to stop enter " + function_stop + " ")
-        while num != function_stop :
+def calculate_average():
+    counter = 0
+    sum = 0
+    num = input('enter num if you want to stop enter {} '.format(FUNCTION_STOP_CHAR))
+    while num != FUNCTION_STOP_CHAR:
+        try:
             sum += float(num)
             counter += 1
             num = input("enter num ")
-        return sum/counter
-    print(calculate_average())
-except:
-    print("bad input")
+        except:
+            print("bad input")
+            num = 0
+            counter -= 1
+    return sum/counter
+
+print(calculate_average())
+
 
 
 
